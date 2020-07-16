8de03c4805a4ac0bfd6b3bbf9007381d9478c623
Jeff <1820779716@qq.com>
2020-07-16 10:39:59 +0800

    author: Jeff 完成listView显示新闻

---
+++ app/src/main/java/com/gxun/mynews/Adapter/HistoryAdapter.java
@@ -0,0 +1,66 @@
+package com.gxun.mynews.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.gxun.mynews.R;
+import com.gxun.mynews.entity.NewsInfo;
+
+import java.util.List;
+
+public class HistoryAdapter extends BaseAdapter {
+
+    List<NewsInfo> newsInfoList;
+
+    public HistoryAdapter(List<NewsInfo> newsInfoList) {
+        this.newsInfoList = newsInfoList;
+    }
+
+    @Override
+    public int getCount() {
+        return newsInfoList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final ViewHolder holder;
+        if(convertView == null){
+            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent,false);
+            holder = new ViewHolder();
+            holder.icon =convertView.findViewById(R.id.icon);
+            holder.title = convertView.findViewById(R.id.title);
+            holder.author = convertView.findViewById(R.id.author);
+            holder.time = convertView.findViewById(R.id.time);
+            convertView.setTag(holder);
+        }else{
+            holder = (ViewHolder) convertView.getTag();
+        }
+        Glide.with(parent.getContext()).load(newsInfoList.get(position).getImgUrl()).into(holder.icon);
+        holder.title.setText(newsInfoList.get(position).getTitle());
+        holder.author.setText("作者："+newsInfoList.get(position).getAuthor());
+        holder.time.setText("时间："+newsInfoList.get(position).getTime());
+
+        return convertView;
+    }
+    class ViewHolder{
+        ImageView icon;
+        TextView title;
+        TextView author;
+        TextView time;
+    }
+}
